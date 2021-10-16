1e1ac1b31f5f38f38c4d0144053b868dbf2e444e
KovalevTimur <mfallendown@gmail.com>
2021-10-16 22:20:06 +0500

    Domashka2

--- exercise2.py
+++ exercise2.py
@@ -1,4 +1,4 @@
-a=int(input("vvedite znachenie:"))
-b=int(input("vvedite znachenie 2-go peremennogo"))
-print("1 peremennaya", a*0+b," 1 peremennaya=", b*0+a)
+a=int(input("vvedite znachenie 1-go peremennogo: "))
+b=int(input("vvedite znachenie 2-go peremennogo: "))
+print("1 peremennaya= ", a*0+b," 2 peremennaya= ", b*0+a)
 
